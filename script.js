deadc712023aaece41008c2e8f5c15373c28927d
azarin_official <108702260+azukiba1005@users.noreply.github.com>
2025-11-11 14:32:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,15 +17,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-(function() {
-    var ua = navigator.userAgent;
-    if (
-        ua.indexOf('iPhone') > 0 || 
-        ua.indexOf('iPad') > 0 || 
-        ua.indexOf('iPod') > 0 || 
-        (ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)
-    ) {
-        // スマホなら 'mobile/index.html' に飛ばす
-        location.href = 'mobile/mobile.html';
-    }
-})();
